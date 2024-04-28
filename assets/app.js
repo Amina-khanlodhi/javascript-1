ebaa16134c82e1e18d547cb6adb18df07edb4db8
Amina-Khanlodhi <aminakhanlodhi331@gmail.com>
2024-04-28 22:15:33 +0500

    javascropt 1 project

---
+++ assets/app.js
@@ -0,0 +1,6 @@
+alert("This is a message");
+alert(" Error! Please enter a valid pawword");
+alert("Welcome to JS land.. Happy coding!");
+alert("Welcome to JS land");
+alert("Happy Coding! prevent this page from creating addtional dialouges");
+alert("Hello!....I can run JS through my web brouser's console");
