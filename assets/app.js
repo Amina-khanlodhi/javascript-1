f8fd3ed52fec323701ad87ca044dc6e70d6a97b4
Amina_Khanlodhi <aminakhanlodhi331@gmail.com>
2024-05-05 23:56:24 +0500

    javascript variables and strings

--- assets/app.js
+++ assets/app.js
@@ -1,6 +1,33 @@
-alert("This is a message");
-alert(" Error! Please enter a valid pawword");
-alert("Welcome to JS land.. Happy coding!");
-alert("Welcome to JS land");
-alert("Happy Coding! prevent this page from creating addtional dialouges");
-alert("Hello!....I can run JS through my web brouser's console");
+// alert("This is a message");
+// alert(" Error! Please enter a valid password");
+// alert("Welcome to JS land.. Happy coding!");
+// alert("Welcome to JS land");
+// alert("Happy Coding! prevent this page from creating addtional dialouges");
+// alert("Hello!....I can run JS through my web brouser's console"); 
+let username = "";
+const myName = "Amina";
+var message
+var message = "Hello World";
+alert ("message");
+
+// Students Bio data 
+var studentName ="John Doe";
+var studentAge = " 20";
+var studentGrade ="A"
+
+// Display bio data in alert boxes
+ alert( studentName);
+ alert( studentAge);
+ alert( studentGrade);
+//   var word = "Pizza";
+//   var alertmessage ="";
+//   for (var i = word.length; i >0;i--){
+//     alertmessage += word.slice(0,i)+ "\n";
+//   }
+// alertmessage(word);
+
+var email = " amiankhanlodhi331@gmail.com";
+alert(" amiankhanlodhi331@gmail.com " );
+
+ var book= " A smarter way to learn javscript";
+ alert(" A smarter way to learn javscript");
